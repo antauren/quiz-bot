f3eaa119c8b88ef61a9964d162254a06f95414b6
Anatoly <antauren@yandex.ru>
2020-04-03 16:34:18 +0300

    now we can use one db for tg- and vk- bots

--- handlers/tg_bot.py
+++ handlers/tg_bot.py
@@ -3,8 +3,14 @@ import handlers.quiz as handler
 CHOOSING = 1
 
 
+def add_tg_prefix(id):
+    return 'tg_{}'.format(id)
+
+
 def handle_surrender(bot, update, redis_db):
-    message = handler.handle_surrender(update.message.from_user.id, redis_db)
+    tg_user_id = add_tg_prefix(update.message.from_user.id)
+
+    message = handler.handle_surrender(tg_user_id, redis_db)
 
     update.message.reply_text(message)
 
@@ -12,7 +18,9 @@ def handle_surrender(bot, update, redis_db):
 
 
 def handle_stat_request(bot, update, redis_db):
-    message = handler.handle_stat_request(update.message.from_user.id, redis_db)
+    tg_user_id = add_tg_prefix(update.message.from_user.id)
+
+    message = handler.handle_stat_request(tg_user_id, redis_db)
 
     update.message.reply_text(message)
 
@@ -20,7 +28,9 @@ def handle_stat_request(bot, update, redis_db):
 
 
 def handle_new_question_request(bot, update, redis_db):
-    message = handler.handle_new_question_request(update.message.from_user.id, redis_db)
+    tg_user_id = add_tg_prefix(update.message.from_user.id)
+
+    message = handler.handle_new_question_request(tg_user_id, redis_db)
 
     update.message.reply_text(message)
 
@@ -28,7 +38,9 @@ def handle_new_question_request(bot, update, redis_db):
 
 
 def handle_solution_attempt(bot, update, redis_db):
-    message = handler.handle_solution_attempt(update.message.text, update.message.from_user.id, redis_db)
+    tg_user_id = add_tg_prefix(update.message.from_user.id)
+
+    message = handler.handle_solution_attempt(update.message.text, tg_user_id, redis_db)
 
     update.message.reply_text(message)
 
--- handlers/vk_bot.py
+++ handlers/vk_bot.py
@@ -1,16 +1,23 @@
 from handlers.quiz import handle_new_question_request, handle_surrender, handle_stat_request, handle_solution_attempt
 
 
+def add_vk_prefix(id):
+    return 'vk_{}'.format(id)
+
+
 def hadle_message(text, user_id, redis_db):
+    vk_user_id = add_vk_prefix(user_id)
+
     if text == 'Новый вопрос':
-        message = handle_new_question_request(user_id, redis_db)
+        message = handle_new_question_request(vk_user_id, redis_db)
+
     elif text == 'Сдаться':
-        message = handle_surrender(user_id, redis_db)
+        message = handle_surrender(vk_user_id, redis_db)
 
     elif text == 'Моя статистика':
-        message = handle_stat_request(user_id, redis_db)
+        message = handle_stat_request(vk_user_id, redis_db)
 
     else:
-        message = handle_solution_attempt(text, user_id, redis_db)
+        message = handle_solution_attempt(text, vk_user_id, redis_db)
 
     return message
